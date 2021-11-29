0cf501816d724a8c27c61ebfba9e4c352d112c42
Austin D <doan.austin.pw@gmail.com>
2021-11-29 14:12:18 -0700

    Added Twitter auth.

--- components/authentication/providers/twitter/TwitterProvider.js
+++ components/authentication/providers/twitter/TwitterProvider.js
@@ -1,12 +1,31 @@
 import Image from "next/image";
-
+import { useRouter } from "next/dist/client/router";
+import { useState } from "react";
+import { TwitterAuthProvider, signInWithPopup } from "@firebase/auth";
+import { auth } from "/libs/firebase";
+import { useAuth } from "libs/hooks/useAuth";
 import { ProviderButton } from "ui/buttons";
-
 import twitter from "./twitter.png";
 
 function TwitterProvider({ children, ...props }) {
+  const [isValidUser, setIsValidUser] = useState(null);
+  const user = useAuth();
+  const router = useRouter();
+  const provider = new TwitterAuthProvider();
+
+  async function signIn() {
+    setIsValidUser(await signInWithPopup(auth, provider));
+  }
+
+  function handleClick() {
+    signIn();
+  }
+
+  if (isValidUser) {
+    router.push("/todo");
+  }
   return (
-    <ProviderButton>
+    <ProviderButton {...props} onClick={handleClick}>
       <div>
         <Image
           src={twitter}
