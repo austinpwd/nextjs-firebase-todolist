915e8b12e3a089044d988e30037b004c326bc628
Austin D <doan.austin.pw@gmail.com>
2021-11-29 14:24:51 -0700

    Fixed component errors in empty templates.

---
+++ pages/privacy.js
@@ -0,0 +1,23 @@
+import { useAuth } from "/libs/hooks/useAuth";
+import { AppBar } from "/components/appbar";
+import { Title } from "/ui/title";
+
+function Privacy(props) {
+  const user = useAuth();
+
+  if (!user) {
+    <>
+      <AppBar />
+      <Title>YOURE NOT WELCOME HERE. GO AWAY</Title>
+    </>;
+  }
+
+  return (
+    <>
+      <AppBar />
+      <Title>Render To-Do List</Title>
+    </>
+  );
+}
+
+export default Privacy;
